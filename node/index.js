3790cd45ee56ab4bd52b67b9f0d133b127319e5e
Maciej Przybylski <42326412+cyvn7@users.noreply.github.com>
2024-12-31 17:49:43 +0100

    -dodano logowanie -hasla od teraz sa hashowane note: docker tez musi miec zainstalowanego bcrypta zeby dzialal

--- node/index.js
+++ node/index.js
@@ -3,6 +3,7 @@ import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import fs from 'fs';
 import path from 'path';
+import bcrypt from 'bcrypt';
 
 const app = express();
 app.use(express.json());
@@ -49,12 +50,17 @@ app.get('/api/all', async (req, res) => {
   }
 });
 
+
 // POST method to insert a user
 app.post('/api/register', async (req, res) => {
   try {
+    //var bcrypt = require('bcryptjs');
     const { name, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10); // Generate salt with 10 rounds
+    const hashedPassword = await bcrypt.hash(password, salt); // Hash the password with the salt
+
     const db = await dbPromise;
-    await db.run(`INSERT INTO users (name, email, password) VALUES (?, ?, ?)`, [name, email, password]);
+    await db.run(`INSERT INTO users (name, email, password) VALUES (?, ?, ?)`, [name, email, hashedPassword]);
     res.status(200).send(req.body);
   } catch (error) {
     if (error.code === 'SQLITE_CONSTRAINT') {
@@ -79,6 +85,29 @@ app.delete('/api/user/:id', async (req, res) => {
     const db = await dbPromise;
     await db.run(`DELETE FROM users WHERE id = ?`, id);
     res.status(200).send(`User with ID ${id} deleted`);
+  } catch (error) {
+    res.status(500).send('Error');
+    console.log(error);
+  }
+});
+
+// POST method to login a user
+app.post('/api/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const db = await dbPromise;
+    const user = await db.get(`SELECT password FROM users WHERE email = ?`, email);
+
+    if (!user) {
+      return res.status(400).send('Invalid email or password');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).send('Invalid email or password');
+    }
+
+    res.status(200).send('Login successful');
   } catch (error) {
     res.status(500).send('Error');
     console.log(error);
